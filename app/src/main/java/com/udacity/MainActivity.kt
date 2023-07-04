b7b0fad3860c777316e63c55d7230a26997b6226
luanpramos <luanpramos@gmail.com>
2023-07-04 12:21:10 -0300

    Create notification methods

--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.udacity
 
+import android.annotation.SuppressLint
 import android.app.DownloadManager
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.graphics.Color
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
@@ -22,6 +26,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var notificationManager: NotificationManager
     private lateinit var pendingIntent: PendingIntent
     private lateinit var action: NotificationCompat.Action
+    private val NOTIFICATION_ID = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +40,17 @@ class MainActivity : AppCompatActivity() {
 //        binding.custom_button.setOnClickListener {
 //            download()
 //        }
+
+        // Initialize notification manager, pending intent, and action
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Create the notification channel
+        createChannel(CHANNEL_ID, "loadApp")
+
+        // Show the notification
+        showNotification()
+
+
     }
 
     private val receiver = object : BroadcastReceiver() {
@@ -61,5 +77,66 @@ class MainActivity : AppCompatActivity() {
         private const val URL =
             "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
         private const val CHANNEL_ID = "channelId"
+    }
+
+    private fun createChannel(channelId: String, channelName: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_HIGH
+            ).apply {
+                setShowBadge(false)
+                enableLights(true)
+                lightColor = Color.RED
+                enableVibration(true)
+                description = getString(R.string.notification_description)
+            }
+
+            notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+    }
+
+    @SuppressLint("UnspecifiedImmutableFlag")
+    private fun showNotification() {
+        val contentIntent = Intent(applicationContext, MainActivity::class.java)
+
+        val contentPendingIntent = PendingIntent.getActivity(
+            applicationContext,
+            NOTIFICATION_ID,
+            contentIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val bigTextStyle = NotificationCompat.BigTextStyle()
+
+        val checkStatusIntent = Intent(applicationContext, DetailActivity::class.java)
+        val checkStatusPendingIntent = PendingIntent.getActivity(
+            applicationContext,
+            0,
+            checkStatusIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val actionCheckStatus = NotificationCompat.Action.Builder(
+            0,
+            "Check the status",
+            checkStatusPendingIntent
+        ).build()
+
+        val builder = NotificationCompat.Builder(
+            applicationContext,
+            CHANNEL_ID
+        )
+            .setSmallIcon(R.drawable.ic_assistant_black_24dp)
+            .setContentTitle(getString(R.string.notification_title))
+            .setContentText(getString(R.string.notification_description))
+            .setContentIntent(contentPendingIntent)
+            .setAutoCancel(true)
+            .setStyle(bigTextStyle)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .addAction(actionCheckStatus)
+        notificationManager.notify(NOTIFICATION_ID, builder.build())
     }
 }

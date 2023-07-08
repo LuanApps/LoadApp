eab69e3a96328bff4ea69e3664a433b96bba5c19
luanpramos <luanpramos@gmail.com>
2023-07-07 21:45:32 -0300

    Done Download method with notification

--- app/src/main/java/com/udacity/DetailActivity.kt
+++ app/src/main/java/com/udacity/DetailActivity.kt
@@ -1,9 +1,11 @@
 package com.udacity
 
+import android.app.NotificationManager
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.udacity.databinding.ActivityDetailBinding
-
 class DetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailBinding
@@ -13,5 +15,23 @@ class DetailActivity : AppCompatActivity() {
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+
+        val action = intent.action
+        if (action == "CHECK_STATUS") {
+            val notificationId = intent.getIntExtra("notification_id", 0)
+            val notificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.cancel(notificationId)
+        }
+
+        val fileName = intent.getStringExtra("file_name").toString()
+        val isSuccess = intent.getBooleanExtra("file_status", false)
+
+        Log.i("DetailActivity", "File Name: $fileName")
+        Log.i("DetailActivity", "Success Status: $isSuccess")
+
+        binding.contentDetail.filename.text = fileName
+        binding.contentDetail.status.text = if (isSuccess) "Success" else "Failure"
     }
+
 }
--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -9,13 +9,22 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.graphics.Color
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import com.udacity.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,40 +37,100 @@ class MainActivity : AppCompatActivity() {
     private lateinit var action: NotificationCompat.Action
     private val NOTIFICATION_ID = 0
 
+    private var git_url: String? = null
+    private var git_filename: String? = null
+    lateinit var loadingButton: Button
+    lateinit var radioGroup: RadioGroup
+    val PERMISSION_REQUEST_CODE = 117
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
 
+        if (Build.VERSION.SDK_INT > 32 && !shouldShowRequestPermissionRationale(android.Manifest.permission.POST_NOTIFICATIONS)) {
+            requestNotificationPermission()
+        }
+
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
-        // TODO: Implement code below
-//        binding.custom_button.setOnClickListener {
-//            download()
-//        }
+        loadingButton = binding.contentMain.customButton
+        radioGroup = binding.contentMain.radioGroup
+        loadingButton.setOnClickListener {
+            if (isInternetAvailable()) {
+                if (radioGroup.checkedRadioButtonId == -1) {
+                    Toast.makeText(this, "Please select the file to download", Toast.LENGTH_SHORT).show()
+                } else {
+                    download()
+                }
+            }
+            else{
+                Toast.makeText(this, "No Internet Connection", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.glide_radio_button -> {
+                    git_url = URL_GLIDE
+                    git_filename = getString(R.string.glide_desc)
+                }
+                R.id.udacity_radio_button -> {
+                    git_url = URL_LOADAPP
+                    git_filename = getString(R.string.loadapp_desc)
+                }
+                R.id.retrofit_radio_button -> {
+                    git_url = URL_RETROFIT
+                    git_filename = getString(R.string.retrofit_desc)
+                }
+            }
+        }
 
         // Initialize notification manager, pending intent, and action
         notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         // Create the notification channel
         createChannel(CHANNEL_ID, "loadApp")
-
-        // Show the notification
-        showNotification()
-
-
     }
 
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+            if (!git_filename.isNullOrEmpty()) {
+                // Check if the download was successful
+                val downloadManager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+                val query = DownloadManager.Query().setFilterById(id!!)
+                val cursor = downloadManager.query(query)
+                if (cursor.moveToFirst()) {
+                    val statusColumnIndex = cursor.getColumnIndex(DownloadManager.COLUMN_STATUS)
+                    if (statusColumnIndex != -1) {
+                        val status = cursor.getInt(statusColumnIndex)
+                        if (status == DownloadManager.STATUS_SUCCESSFUL) {
+                            showDownloadNotification(git_filename!!, true)
+                            Log.i(
+                                "status_download",
+                                git_filename + " Download status: " + true.toString()
+                            )
+                        } else {
+                            showDownloadNotification(git_filename!!, false)
+                            Log.i(
+                                "status_download",
+                                git_filename + " Download status: " + false.toString()
+                            )
+                        }
+                    } else {
+                        Log.i("download_fail", "deu ruim!")
+                        // Handle case when the status column is not found
+                    }
+                }
+            }
         }
     }
 
     private fun download() {
         val request =
-            DownloadManager.Request(Uri.parse(URL))
+            DownloadManager.Request(Uri.parse(git_url))
                 .setTitle(getString(R.string.app_name))
                 .setDescription(getString(R.string.app_description))
                 .setRequiresCharging(false)
@@ -74,8 +143,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        private const val URL =
+        private const val URL_LOADAPP =
             "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+        private const val URL_GLIDE = "https://github.com/bumptech/glide"
+        private const val URL_RETROFIT = "https://github.com/square/retrofit"
         private const val CHANNEL_ID = "channelId"
     }
 
@@ -99,31 +170,28 @@ class MainActivity : AppCompatActivity() {
     }
 
     @SuppressLint("UnspecifiedImmutableFlag")
-    private fun showNotification() {
-        val contentIntent = Intent(applicationContext, MainActivity::class.java)
+    private fun showDownloadNotification(fileName: String, isSuccess: Boolean) {
+        val contentIntent = Intent(this@MainActivity, DetailActivity::class.java).apply {
+            putExtra("file_name", fileName)
+            putExtra("file_status", isSuccess)
+            action = "CHECK_STATUS"
+            putExtra("notification_id", NOTIFICATION_ID)
+        }
 
-        val contentPendingIntent = PendingIntent.getActivity(
+        pendingIntent = PendingIntent.getActivity(
             applicationContext,
             NOTIFICATION_ID,
             contentIntent,
             PendingIntent.FLAG_UPDATE_CURRENT
         )
 
-        val bigTextStyle = NotificationCompat.BigTextStyle()
-
-        val checkStatusIntent = Intent(applicationContext, DetailActivity::class.java)
-        val checkStatusPendingIntent = PendingIntent.getActivity(
-            applicationContext,
-            0,
-            checkStatusIntent,
-            PendingIntent.FLAG_UPDATE_CURRENT
-        )
-
-        val actionCheckStatus = NotificationCompat.Action.Builder(
+        action = NotificationCompat.Action.Builder(
             0,
             "Check the status",
-            checkStatusPendingIntent
+            pendingIntent
         ).build()
+
+        val bigTextStyle = NotificationCompat.BigTextStyle()
 
         val builder = NotificationCompat.Builder(
             applicationContext,
@@ -132,11 +200,43 @@ class MainActivity : AppCompatActivity() {
             .setSmallIcon(R.drawable.ic_assistant_black_24dp)
             .setContentTitle(getString(R.string.notification_title))
             .setContentText(getString(R.string.notification_description))
-            .setContentIntent(contentPendingIntent)
+            .setContentIntent(pendingIntent)
             .setAutoCancel(true)
             .setStyle(bigTextStyle)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
-            .addAction(actionCheckStatus)
+            .addAction(action)
+
         notificationManager.notify(NOTIFICATION_ID, builder.build())
+    }
+
+    private fun isInternetAvailable(): Boolean {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val network = connectivityManager.activeNetwork ?: return false
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
+        return networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+    }
+
+
+    private fun requestNotificationPermission() {
+        if (Build.VERSION.SDK_INT > 32) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.POST_NOTIFICATIONS),
+                PERMISSION_REQUEST_CODE
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission granted
+                } else {
+                    // Permission denied
+                }
+            }
+        }
     }
 }
